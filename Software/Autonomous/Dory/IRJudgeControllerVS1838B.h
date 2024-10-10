d9daa42a4d9aed0629b136ddaa026d73e54b5250
Ana Luiza Fioravante Di Sciascio <analuiza197@gmail.com>
2024-10-10 17:16:47 -0300

    fix: fix IR judge controller buttons

--- Software/Autonomous/Dory/IRJudgeControllerVS1838B.h
+++ Software/Autonomous/Dory/IRJudgeControllerVS1838B.h
@@ -13,27 +13,34 @@ void checkSensorIR() {
     Serial.println(IrReceiver.decodedIRData.command, HEX);
 
     switch (IrReceiver.decodedIRData.command) {
-      // Teste do led para sabermos se o IR está funcionando
+      case 0:
+        Serial.println("Apertou o botão 1");
+        break;
       case 0x1:
       case 0x11:
-        Serial.println("Apertou o botão 1");
+        Serial.println("Apertou o botão 2");
+        isRunning = true;
         break;
       // Inicializa a estratégia simples
       case 0x2:
       case 0x12:
-        Serial.println("Apertou o botão 2");
-        isRunning = true;
-        break;
-      // Finaliza o loop
-      case 0x3:
-      case 0x13:
         Serial.println("Apertou o botão 3");
+        Serial.println(" Robô Morreu ✖╭╮✖");
         while (1) {
           isRunning = false;
           velMotorL = 0;
           velMotorR = 0;
           motorsOutput();
         }
+        break;
+      // Finaliza o loop
+      case 0x3:
+      case 0x13:
+        Serial.println("Apertou o botão 4");
+        isRunning = false;
+        velMotorL = 0;
+        velMotorR = 0;
+        motorsOutput();
         break;
     }
 
